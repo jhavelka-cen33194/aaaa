8819feefae6d782e47cad38453ea62f757eed215
Jiri Havelka <jhavelka@csas.cz>
2024-04-10 11:05:48 +0200

    a

--- renovate.js
+++ renovate.js
@@ -9,7 +9,7 @@ module.exports = {
   //repositories: [process.env.GITHUB_REPOSITORY], // use the GITHUB_REPOSITORY environment variable
   gitUrl: 'git@github.com:csas-dev/inet-terraform-ghb-modul-github-files.git',
   packageRules: [
-    {
+    /*{
       description: 'lockFileMaintenance',
       matchUpdateTypes: [
         'pin',
@@ -21,14 +21,12 @@ module.exports = {
       ],
       dependencyDashboardApproval: false,
       stabilityDays: 0,
-    },
+    },*/
     {
       //"matchPackageNames": ["node"],
       "groupName": "GitHub Actions",
       "matchManagers": ["github-actions"],
       "fileMatch": [
-        "(^|/)(workflow-templates|\\.(?:github|gitea|forgejo)/(?:workflows|actions))/.+\\.ya?ml$",
-        "(^|/)action\\.ya?ml$",
         "(^|/)\\files/(?:dev|ops)/(?:be|fe|terraform)/(?:LAF/)?.+\\.ya?ml$",
       ]
     },
